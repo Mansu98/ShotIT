4878d2835231ccb4c0786ce6a0d0df2dbedcab48
unknown <mansubajracharya09@gmail.com>
2021-08-06 10:28:45 +0545

    backend completed

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,15 +1,31 @@
 const express= require("express");
 const router = express.Router();
 
-const {getProducts,newProduct, getSingleProduct, updateProduct, deleteProduct}= require("../controllers/productController");
+const {
+    getProducts,
+    newProduct, 
+    getSingleProduct,
+     updateProduct, 
+     deleteProduct,
+     createProductReview,
+     getProductReviews,
+     deleteReview
+    }= require("../controllers/productController");
 
 const {isAuthenticatedUser, authorizeRoles} = require("../middlewares/auth");
 
 
 router.route("/products").get(getProducts);
-
 router.route("/product/:id").get(getSingleProduct);
 
+ 
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+router.route("/reviews")
+.get(isAuthenticatedUser, getProductReviews)
+.delete(isAuthenticatedUser, deleteReview)
+
+
+//admin
 router.route("/admin/product/:id").put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct);
 
 router.route("/admin/product/:id").delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct);
